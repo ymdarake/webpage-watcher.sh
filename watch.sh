231d986f9acb29c0a23702e4dc32e38bce38f9d4
ymdarake <sakamoto.takumi.dev@gmail.com>
2024-04-08 22:02:29 +0900

    :recycle: 再帰をwhileにして平たく改善

--- watch.sh
+++ watch.sh
@@ -3,19 +3,20 @@
 function watch_while_exists() {
   local url=$1
   local needle=$2
-
-  result=$(curl -H "Accept-Language: ja-JP,ja;q=0.9,en;q=0.8" "$url")
-  
-  case "$result" in
-    *"$needle"*)
-      echo not found
-      echo "$result" > result.txt
-      sleep 60
-      watch_while_exists "$url" "$needle"
-      ;;
-    *)
-      echo found!
-      echo "$result" > result.txt
-      ;;
-  esac
+  while true
+  do
+    result=$(curl -H "Accept-Language: ja-JP,ja;q=0.9,en;q=0.8" "$url")
+    case "$result" in
+      *"$needle"*)
+        echo not found
+        echo "$result" > result.txt
+        sleep 60
+        ;;
+      *)
+        echo found!
+        echo "$result" > result.txt
+        break
+        ;;
+    esac
+  done
 }
