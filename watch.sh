c35b57940407f0f3da9e08120f35b8fc5058ddc1
ymdarake <sakamoto.takumi.dev@gmail.com>
2024-04-08 22:32:16 +0900

    :pencil2: chore: 判定対象の有無のecho文言変更

--- watch.sh
+++ watch.sh
@@ -8,12 +8,12 @@ function watch_while_exists() {
     result=$(curl -H "Accept-Language: ja-JP,ja;q=0.9,en;q=0.8" "$url")
     case "$result" in
       *"$needle"*)
-        echo not found
+        echo still exists
         echo "$result" > result.txt
         sleep 60
         ;;
       *)
-        echo found!
+        echo disappeared!
         echo "$result" > result.txt
         break
         ;;
